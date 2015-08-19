dfc980667efdf8c4bb20e007aa9daee1325f9ec3
Claudiu Tarta <ctarta1@sheffield.ac.uk>
2015-08-19 18:41:47 +0100

    imported users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  def self.import
+    users = Rails.root.join('db').join('users.csv')
+
+    CSV.foreach(users, :headers => true) do |row_user|
+
+      user = User.new
+      user.ucard_no = "00" + row_user["ucard_no"]
+      user.surname = row_user["surname"]
+      user.forename = row_user["forename"]
+      user.job_title = row_user["job_title"]
+      user.admin = false
+      user.save
+    end
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818102456) do
+ActiveRecord::Schema.define(version: 20150819173646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150818102456) do
     t.string   "job_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "admin"
   end
 
 end

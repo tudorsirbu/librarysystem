81adda91f4c4d275a14c49fe3b934e4000285cbb
Tudor Sirbu <tudor@k-now.co.uk>
2015-10-15 00:01:03 +0100

    fixed problems with import

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -79,7 +79,12 @@ module ItemsHelper
       puts response
       book = response['items'].first['volumeInfo']
       item.title = book['title'] unless book['title'].nil? || book['title'].empty?
-      item.year = Time.parse(book['publishedDate']).year unless  book['publishedDate'].nil? || book['publishedDate'].empty?
+
+      if book['publishedDate'].size == 4
+        item.year = book['publishedDate']
+      else
+        item.year = Time.parse(book['publishedDate']).year unless  book['publishedDate'].nil? || book['publishedDate'].empty?
+      end
       item.category = Category.find_or_create_by(name: book['categories'].first) unless book['categories'].nil? || book['categories'].empty?
       item.thumbnail = book['imageLinks']['smallThumbnail'] unless book['imageLinks']['smallThumbnail'].nil? || book['imageLinks']['smallThumbnail'].empty?
     end

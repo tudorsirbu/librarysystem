b03c69a533e77e2bc17ac84dcfa967ca551da5c3
Claudiu Tarta <clau@dyn099016.shef.ac.uk>
2016-01-28 17:00:49 +0000

    fixed cat name

--- app/models/item.rb
+++ app/models/item.rb
@@ -52,7 +52,11 @@ class Item < ActiveRecord::Base
 
   def to_label
     if self.category_id
-      "#{self.barcode || 'Missing barcode'} - #{self.title || 'Missing title'} - #{self.category.name || 'Missing category name'}"
+      if self.category.name
+        "#{self.barcode || 'Missing barcode'} - #{self.title || 'Missing title'} - #{self.category.name}"
+      else
+        "#{self.barcode || 'Missing barcode'} - #{self.title || 'Missing title'} - Missing category name"
+      end
     else
       "#{self.barcode || 'Missing barcode'} - #{self.title || 'Missing title'}"
     end
